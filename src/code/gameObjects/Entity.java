00f2797f86f40870a98a96a268a7372fb60ec5a4
rebecanoya <rebecanoyait@gmail.com>
2022-05-04 13:42:11 +0200

    position new

--- src/code/gameObjects/Entity.java
+++ src/code/gameObjects/Entity.java
@@ -13,22 +13,26 @@ public class Entity extends GameObject {
     public ArrayList<Vector2> lastPositionList;
 
     public boolean move(Vector2 pos) {
+
+        Vector2 newPosition = new Vector2(pos.x + position.x, pos.y + position.y);
         
-        if (MapController.gameObjects[pos.x+position.x][pos.y+position.y]==null) {
-            Vector2 lastPosition= new Vector2(position.x, position.y);
-            position= new Vector2(pos.x+position.x, pos.y+position.y);
-            MapController.gameObjects[pos.x][pos.y]=this;
-            MapController.gameObjects[lastPosition.x][lastPosition.y]=null;
+        
+        
+        if (MapController.gameObjects[newPosition.x][newPosition.y] == null) {
+            Vector2 lastPosition = new Vector2(position.x, position.y);
+            position = newPosition;
+            MapController.gameObjects[pos.x][pos.y] = this;
+            MapController.gameObjects[lastPosition.x][lastPosition.y] = null;
             return true;
         }
-        
+
         return false;
 
     }
 
     public Entity(Vector2 position, GameObjectType objectType, int sep) {
         super(position, objectType, sep);
-        
+
     }
 
 }
