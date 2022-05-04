dbef0740408b203f9f2e53fcbd49adf5cd7c4bec
iago-pl <iagopenalorenzo@gmail.com>
2022-05-04 12:58:43 +0200

    Update Main.java

--- src/code/main/Main.java
+++ src/code/main/Main.java
@@ -11,7 +11,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("SI LEES ESTO RECUERDAME DARLE UN PUÑETAZO A REBECA");
         JFrame window = new JFrame();
         window.setResizable(false);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
