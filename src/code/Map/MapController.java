1d78a0112d75867a47b8d23c84278fd0288354ee
iago-pl <iagopenalorenzo@gmail.com>
2022-05-23 10:27:33 +0200

    a

--- src/code/Map/MapController.java
+++ src/code/Map/MapController.java
@@ -11,33 +11,33 @@ import java.util.ArrayList;
  * @author a21rebecanf
  */
 public class MapController {
-    
+
     public Map currentMap = new Map(new MapLayer(), new MapLayer());
-    
+
     ArrayList<MapLayer> snapShots = new ArrayList<>();
-    
+
     boolean loading;
-    
+
     private Vector2 playerLastPos;
-    
+
     public MapController() {
         loading = true;
         loadMap();
     }
-    
+
     public boolean isLoading() {
         return loading;
     }
-    
+
     private void load() {
-        
+
         snapShots.clear();
         currentMap = new Map(ReferenceController.mapReader.maps.get(0));
-        
+
         for (int i = 0; i < currentMap.playground.level.length; i++) {
             for (int j = 0; j < currentMap.playground.level[0].length; j++) {
                 if (currentMap.playground.level[i][j] != null) {
-                    
+
                     switch (currentMap.playground.level[i][j].objectType) {
                         case PLAYER:
                             ReferenceController.player = (PlayerEntity) currentMap.playground.level[i][j];
@@ -47,55 +47,55 @@ public class MapController {
                         case BOX:
                         case KEY:
                             currentMap.playground.level[i][j].setPosition(new Vector2(i, j));
-                        
+
                     }
                 }
             }
         }
-        
+
         MapLayer temp = new MapLayer(currentMap.playground);
         snapShots.add(temp);
-        
+
         loading = false;
     }
-    
+
     public void loadMap() {
         //cargar mapa
         loading = true;
-        
+
         if (ReferenceController.infoController != null) {
             ReferenceController.mapReader.maps.remove(0);
         }
-        
+
         load();
-        
+
         if (ReferenceController.infoController != null) {
             ReferenceController.dbController.insertLevelCompleted(ReferenceController.dbController.hashList.get(ReferenceController.infoController.getLevel()), ReferenceController.infoController.getStepsCurrentLevel() + 1);
             ReferenceController.infoController.increaseLevel();
             ReferenceController.infoController.resetCurrentStepCount();
         }
     }
-    
+
     public void reloadMap() {
         //cargar mapa
         loading = true;
-        
+
         load();
     }
-    
+
     public void takeSnapshot() {
-        
+
         if (playerLastPos.x != ReferenceController.player.position.x || playerLastPos.y != ReferenceController.player.position.y) {
-            
+
             MapLayer temp = new MapLayer(currentMap.playground);
-            
+
             snapShots.add(temp);
-            
+
             playerLastPos = ReferenceController.player.position;
-            
+
         }
     }
-    
+
     public void loadSnapshot() {
         if (snapShots.size() > 1) {
             ReferenceController.audioController.play(1);
@@ -104,7 +104,7 @@ public class MapController {
             for (int i = 0; i < currentMap.playground.level.length; i++) {
                 for (int j = 0; j < currentMap.playground.level[0].length; j++) {
                     if (currentMap.playground.level[i][j] != null) {
-                        
+
                         currentMap.playground.level[i][j].setPosition(new Vector2(i, j));
                     }
                 }
